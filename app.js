6b431c9f94badbbf9d89ea57adb05511f7695792
MikeInDaHouse <88990416+MikeInDaHouse@users.noreply.github.com>
2024-03-26 17:41:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate')
+
+
+
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+
+
+
+app.use('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
