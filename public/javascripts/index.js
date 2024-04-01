212bb8306341e93cdb96997414f0f92be84cd370
MikeInDaHouse <88990416+MikeInDaHouse@users.noreply.github.com>
2024-03-31 17:22:22 -0700

    3rd commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,39 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("Individual-services-button");
+    var section = document.getElementById("Individual-services");
+  
+    button.addEventListener("click", function() {
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("Basic-cleaning-button");
+    var section = document.getElementById("Basic-cleaning");
+  
+    button.addEventListener("click", function() {
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("Full-detal-button");
+    var section = document.getElementById("Full-detail");
+  
+    button.addEventListener("click", function() {
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("Contact-button");
+    var section = document.getElementById("Contact");
+  
+    button.addEventListener("click", function() {
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+
+ 
